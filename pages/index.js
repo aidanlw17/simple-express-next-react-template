cc31295a31a8961d6d1107b74d6890bbce934d21
Aidan Lawford-Wickham <aidan.lw17@gmail.com>
2019-11-18 14:15:03 -0500

    Add basic frontend

---
+++ pages/index.js
@@ -0,0 +1,19 @@
+import Layout from '../components/Layout';
+
+const Index = () => (
+	<Layout>
+		<h1>Welcome to the index page!</h1>
+		<p>Do whatever you want with this template!</p>
+		<style jsx>{`
+			h1 {
+				font-family: 'Arial';
+			}
+
+			body {
+				background-color: #524840;
+			}
+		`}</style>
+	</Layout>
+);
+
+export default Index;
